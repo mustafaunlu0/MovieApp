866f58e7206edfad89d6a20ab11459658f97d1b4
mustafaunlu0 <79142435+mustafaunlu0@users.noreply.github.com>
2022-09-25 15:17:11 +0300

    Splash Screen design.

--- app/src/main/java/com/mustafaunlu/movieapp/MainActivity.kt
+++ app/src/main/java/com/mustafaunlu/movieapp/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.mustafaunlu.movieapp
 
+import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
+import android.view.animation.AnticipateInterpolator
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val splashScreen =installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
+
+        val content :View =findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnDrawListener {
+            // sleep is added for now
+            Thread.sleep(3000)
+            object  : ViewTreeObserver.OnPreDrawListener{
+
+                override fun onPreDraw(): Boolean {
+                    //ViewModel ready will add
+                    return if(true){
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    }else{
+                        false
+
+                    }
+                }
+
+
+            }
+        }
+
+
+
     }
 }
